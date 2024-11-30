8de5ab194f43115569d3067b65c96d116e458dce
Huy Dang <ghuydang@gmail.com>
2024-11-30 01:15:07 -0800

    update dock

--- hosts/darwin/muninn.nix
+++ hosts/darwin/muninn.nix
@@ -52,15 +52,10 @@ let username = user.username; in
   # Fully declarative dock using the latest from Nix Store
   local.dock.enable = true;
   local.dock.entries = [
-    { path = "/Applications/Slack.app/"; }
+    { path = "/System/Cryptexes/App/System/Applications/Safari.app"; }
     { path = "/System/Applications/Messages.app/"; }
-    { path = "/System/Applications/Facetime.app/"; }
-    { path = "/System/Applications/Music.app/"; }
-    { path = "/System/Applications/News.app/"; }
-    { path = "/System/Applications/Photos.app/"; }
-    { path = "/System/Applications/Photo Booth.app/"; }
-    { path = "/System/Applications/TV.app/"; }
-    { path = "/System/Applications/Home.app/"; }
+    { path = "/System/Applications/iPhone Mirroring.app/"; }
+    { path = "/Applications/Obsidian.app"; }
     {
       path = "${config.users.users.${username}.home}/Downloads";
       section = "others";

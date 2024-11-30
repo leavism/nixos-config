e0563b6513fdd9059c5ffc952e54a2596a1a654f
Huy Dang <ghuydang@gmail.com>
2024-11-29 23:20:18 -0800

    initial commit
    update dock
    
    add zen and zed casks

---
+++ hosts/darwin/common/default.nix
@@ -0,0 +1,113 @@
+{ config, pkgs, lib, user,... }:
+
+let username = user.username; in
+{
+  imports = [
+    ../../shared
+  ];
+
+  services.nix-daemon.enable = true;
+
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      trusted-users = [ "@admin" "${username}" ];
+      substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+    };
+
+    gc = {
+      user = "root";
+      automatic = true;
+      interval = { Weekday = 0; Hour = 2; Minute = 0; };
+      options = "--delete-older-than 30d";
+    };
+
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    # System packages here
+    # For macOS hosts, keep this to only terminal packages.
+    # GUI packages will be installed semi-permanently and removing them is tedious.
+    dockutil # declarative
+  ] ++ (import ../../shared/packages.nix { inherit pkgs; });
+
+
+  # It me
+  users.users.${username} = {
+    name = "${username}";
+    home = "/Users/${username}";
+    isHidden = false;
+    shell = pkgs.zsh;
+  };
+
+  homebrew = {
+    enable = lib.mkForce true;
+    onActivation.cleanup = lib.mkDefault "zap";
+    casks = [
+      "iterm2"
+      "visual-studio-code"
+      "zed"
+      "zen-browser"
+
+      "zoom"
+      "slack"
+      "splashtop-business"
+      "microsoft-teams"
+      "craft"
+      "1password"
+
+      "bitwarden"
+      "figma"
+      "discord"
+
+      "adguard"
+      "transmission"
+      "sensei"
+      "keka"
+      "cleanshot"
+      "raycast"
+      "homerow"
+      "aerospace"
+      "obsidian"
+    ];
+    masApps = {
+      "Things" = 904280696;
+      "Goodnotes 6" = 1444383602;
+
+      "Fantastical" = 975937182;
+      "Microsoft OneNote" = 784801555;
+      "OneDrive" = 823766827;
+      "Microsoft Excel" = 462058435;
+      "Microsoft Outlook" = 985367838;
+      "Microsoft Word" = 462054704;
+      "Microsoft PowerPoint" = 462062816;
+    };
+  };
+
+  # Enable home-manager
+  home-manager = {
+    useGlobalPkgs = true;
+    users.${username} = { pkgs, config, lib, ... }:{
+    home = {
+      enableNixpkgsReleaseCheck = false;
+      packages = pkgs.callPackage ../../shared/packages.nix {};
+      stateVersion = "23.11";
+    };
+    programs = {
+      # Overwrite and extend the shared home-manager configurations here
+      # Example:
+      # git = darwinHomeManager.git // { # Don't forget to merge into darwinHomeManager.git
+      #   userName = "overrideUserName";  # This overrides the shared config
+      # };
+    } // import ../../shared/home-manager.nix { inherit config pkgs lib user; };
+
+    # Marked broken Oct 20, 2022 check later to remove this
+    # https://github.com/nix-community/home-manager/issues/3344
+    manual.manpages.enable = false;
+    };
+  };
+}
---
+++ hosts/darwin/odin.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, user,... }:
+
+let username = user.username; in
+{
+  imports = [
+    ./common
+    ./common/dock
+  ];
+
+  # === Common Homebrew Configuration Overwrite ===
+  # Check what the defaults are in ./common/default.nix
+  homebrew = {
+    casks = [
+      # Extends the cask list in the common configuration
+    ];
+    masApps = {
+      # Extends the cask list in the common configuration
+      # Quickly look up Mac App Store names and IDs with:
+      # $ nix shell nixpkgs#mas
+      # $ mas search <app name>
+      "Final Cut Pro" = 424389933;
+      "Compressor" = 424390742;
+    };
+  };
+
+  system = {
+    stateVersion = 4;
+    checks.verifyNixPath = false;
+
+    defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        ApplePressAndHoldEnabled = false;
+
+        KeyRepeat = 2; # Values: 120, 90, 60, 30, 12, 6, 2
+        InitialKeyRepeat = 15; # Values: 120, 94, 68, 35, 25, 15
+
+        "com.apple.mouse.tapBehavior" = 1;
+        "com.apple.sound.beep.volume" = 0.0;
+        "com.apple.sound.beep.feedback" = 0;
+      };
+
+      dock = {
+        autohide = false;
+        show-recents = false;
+        launchanim = true;
+        orientation = "bottom";
+        tilesize = 48;
+      };
+    };
+  };
+
+  # Fully declarative dock using the latest from Nix Store
+  local.dock.enable = true;
+  local.dock.entries = [
+    { path = "/Applications/Slack.app/"; }
+    { path = "/System/Applications/Messages.app/"; }
+    { path = "/System/Applications/Facetime.app/"; }
+    { path = "/System/Applications/Music.app/"; }
+    { path = "/System/Applications/News.app/"; }
+    { path = "/System/Applications/Photos.app/"; }
+    { path = "/System/Applications/Photo Booth.app/"; }
+    { path = "/System/Applications/TV.app/"; }
+    { path = "/System/Applications/Home.app/"; }
+    {
+      path = "${config.users.users.${username}.home}/Downloads";
+      section = "others";
+      options = "--sort name --view grid --display stack";
+    }
+  ];
+}

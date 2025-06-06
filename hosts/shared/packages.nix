980a9860c59aee5152ee3603b04914167c78b988
Huy Dang <ghuydang@gmail.com>
2025-06-05 21:08:59 -0700

    install tailscale

--- hosts/shared/packages.nix
+++ hosts/shared/packages.nix
@@ -1,24 +1,30 @@
 /*
- * === Nix Packages Shared Cross-Platform ===
+  === Nix Packages Shared Cross-Platform ===
  *
  * For easier macOS compatibility, only put terminal packages. Any GUI packages
  * will be semi-permanent on macOS and is tedious to remove when no longer needed.
  */
 { pkgs }:
 with pkgs; [
-  # Nix packages here
-  git
+  # General development
   zsh
-  chezmoi
+  oh-my-posh
+  git
   tmux
-  neovim
   sesh
   zoxide
   fzf
   gum
-  lazygit
   eza
-  oh-my-posh
-  cargo # For some Mason packages
+  ripgrep
   pyenv
+  tailscale
+  
+  # Configurations
+  chezmoi
+  
+  # Nvim configurations
+  neovim
+  lazygit
+  cargo # For some Mason packages
 ]

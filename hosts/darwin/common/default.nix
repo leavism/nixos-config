c04e946cfe4ab4f6966f141fe65303018324b9e5
Huy Dang <ghuydang@gmail.com>
2025-09-20 13:22:18 -0700

    move macos dock config to the device level

--- hosts/darwin/common/default.nix
+++ hosts/darwin/common/default.nix
@@ -1,10 +1,17 @@
 /*
-=== Configuration for All macOS Devices ===
-*
-* Sensible defaults for all my macOS machines.
+  === Configuration for All macOS Devices ===
+  *
+  * Sensible defaults for all my macOS machines.
 */
-{ pkgs, lib, user,... }:
-let username = user.username; in
+{
+  pkgs,
+  lib,
+  user,
+  ...
+}:
+let
+  username = user.username;
+in
 {
   imports = [
     ../../shared
@@ -47,19 +54,6 @@ let username = user.username; in
         launchanim = true;
         orientation = "left";
         tilesize = 48;
-        persistent-apps = [
-          "/System/Cryptexes/App/System/Applications/Safari.app"
-          "/Applications/Zen.app"
-          "/System/Applications/Messages.app/"
-          "/System/Applications/iPhone Mirroring.app/"
-          "/Applications/Obsidian.app"
-          "/Applications/Todoist.app/"
-        ];
-        
-        persistent-others = [
-          "/Users/${user.username}/Sync"
-          "/Users/${user.username}/Downloads"
-        ];
         # https://mynixos.com/nix-darwin/option/system.defaults.dock.wvous-bl-corner
         wvous-bl-corner = 1;
         wvous-br-corner = 1;
@@ -71,29 +65,41 @@ let username = user.username; in
 
   # === Services ===
   # Syncthing service is in homemanager below
-  
+
   services.tailscale.enable = true;
-  
+
   services.jankyborders = {
     enable = true;
-    hidpi = true;
+    hidpi = false; # September 2025 - True breaks borders
     active_color = "0xFF89B4FA";
     inactive_color = "0x00";
     width = 10.0;
+    order = "above";
+    style = "round";
   };
 
   nix = {
     package = pkgs.nix;
     settings = {
-      trusted-users = [ "@admin" "${username}" ];
-      substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+      trusted-users = [
+        "@admin"
+        "${username}"
+      ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+      ];
       trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
     };
 
     # Garbage collection
     gc = {
       automatic = true;
-      interval = { Weekday = 0; Hour = 2; Minute = 0; };
+      interval = {
+        Weekday = 0;
+        Hour = 2;
+        Minute = 0;
+      };
       options = "--delete-older-than 30d";
     };
 
@@ -102,13 +108,15 @@ let username = user.username; in
     '';
   };
 
-  environment.systemPackages = with pkgs; [
-    # System packages here. 
-    # For macOS hosts, keep this to only terminal packages. GUI packages installed via
-    # are semi-permanently and removing them is tedious.
-    dockutil # declarative
-  ] ++ (import ../../shared/packages.nix { inherit pkgs; });
-
+  environment.systemPackages =
+    with pkgs;
+    [
+      # System packages here.
+      # For macOS hosts, keep this to only terminal packages. GUI packages installed via
+      # are semi-permanently and removing them is tedious.
+      dockutil # declarative
+    ]
+    ++ (import ../../shared/packages.nix { inherit pkgs; });
 
   # It me
   users.users.${username} = {
@@ -122,11 +130,7 @@ let username = user.username; in
     enable = lib.mkForce true;
     onActivation.cleanup = lib.mkDefault "zap";
     brews = [
-      # TODO: Go back to stable release after October 2025
-      { 
-        name = "borders"; 
-        args = "HEAD";
-      }
+      "borders"
       "tpm"
       "gh"
       "nvm"
@@ -134,7 +138,7 @@ let username = user.username; in
     casks = [
       "font-geist-mono-nerd-font"
       "appcleaner"
-      
+
       "ghostty"
       "visual-studio-code"
       "zen"
@@ -175,27 +179,42 @@ let username = user.username; in
   # Enable home-manager
   home-manager = {
     useGlobalPkgs = true;
-    users.${username} = { pkgs, config, lib, ... }:{
-      home = {
-        enableNixpkgsReleaseCheck = false;
-        packages = pkgs.callPackage ../../shared/packages.nix {};
-        stateVersion = "23.11";
-      };
-      programs = {
-        # Overwrite and extend the shared home-manager configurations here
-        # Example:
-        # git = darwinHomeManager.git // { # Don't forget to merge into darwinHomeManager.git
-        #   userName = "overrideUserName";  # This overrides the shared config
-        # };
-      } // import ../../shared/home-manager.nix { inherit config pkgs lib user; };
+    users.${username} =
+      {
+        pkgs,
+        config,
+        lib,
+        ...
+      }:
+      {
+        home = {
+          enableNixpkgsReleaseCheck = false;
+          packages = pkgs.callPackage ../../shared/packages.nix { };
+          stateVersion = "23.11";
+        };
+        programs = {
+          # Overwrite and extend the shared home-manager configurations here
+          # Example:
+          # git = darwinHomeManager.git // { # Don't forget to merge into darwinHomeManager.git
+          #   userName = "overrideUserName";  # This overrides the shared config
+          # };
+        }
+        // import ../../shared/home-manager.nix {
+          inherit
+            config
+            pkgs
+            lib
+            user
+            ;
+        };
 
-      # Marked broken Oct 20, 2022 check later to remove this
-      # https://github.com/nix-community/home-manager/issues/3344
-      manual.manpages.enable = false;
+        # Marked broken Oct 20, 2022 check later to remove this
+        # https://github.com/nix-community/home-manager/issues/3344
+        manual.manpages.enable = false;
 
-      services.syncthing = {
-        enable = true;
+        services.syncthing = {
+          enable = true;
+        };
       };
-    };
   };
 }
